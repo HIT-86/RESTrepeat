c20ea5855990233e6ea12fd60a4ce4431bb4f761
HIT86 <hit86@foxmail.com>
2019-05-16 00:11:49 +0800

    描述：实现post请求的获取，实现自定义用户名、密码、IP、端口以及命令功能，实现可以同时发送两个命令 修改：增加HTTP协议请求头的实现，对用户名和密码使用base64加密处理已通过设备端验证，url部分增加对端口的处理，增加应答框用来显示设备端应答信息，代码重复较多，待优化。
    Signed-off-by: HIT86 <hit86@foxmail.com>

--- Include/MainWindow.h
+++ Include/MainWindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QNetworkAccessManager>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,17 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_toolButton_send1_clicked();
+    void on_toolButton_send2_clicked();
+
+    void NetworkReplySend1(QNetworkReply *reply);
+    void NetworkReplySend2(QNetworkReply *reply);
+
 private:
     Ui::MainWindow *ui;
+    QNetworkAccessManager *m_send1;
+    QNetworkAccessManager *m_send2;
 };
 
 #endif // MAINWINDOW_H
--- Src/MainWindow.cpp
+++ Src/MainWindow.cpp
@@ -1,14 +1,107 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
+#include <QNetworkRequest>
+#include <QNetworkReply>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    m_send1(new QNetworkAccessManager(this)),
+    m_send2(new QNetworkAccessManager(this))
 {
     ui->setupUi(this);
+    
+    connect(m_send1, SIGNAL(finished(QNetworkReply*)), this, SLOT(NetworkReplySend1(QNetworkReply*)));
+    connect(m_send2, SIGNAL(finished(QNetworkReply*)), this, SLOT(NetworkReplySend2(QNetworkReply*)));
+
+    //QNetworkRequest request;
+    //request.setUrl(QUrl("http://www.baidu.com"));
+
+    //m_send1->get(request);
+
+    //QByteArray postData;
+    //postData.append()
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_toolButton_send1_clicked()
+{
+    QNetworkRequest request;
+
+    request.setUrl(QUrl("http://" + ui->lineEdit_IP->text() + ":" + ui->lineEdit_port->text() + ui->lineEdit_package1->text()));
+    request.setRawHeader("Accept", "text/html, */*; q=0.01");
+    request.setRawHeader("Accept-Encoding", "gzip, deflate");
+    request.setRawHeader("Accept-Language", "zh-CN");
+    QString str =  ui->lineEdit_userName->text() + ":" + ui->lineEdit_password->text();
+    request.setRawHeader("Authorization", "Basic " + str.toLocal8Bit().toBase64());
+    request.setRawHeader("Cache-Control", "no-cache");
+    request.setRawHeader("Connection", "Keep-Alive");
+    request.setRawHeader("Content-Length", "0");
+    request.setRawHeader("Content-Type", "application/json; charset=utf-8");
+    request.setRawHeader("Cookie", "webserver_username=admin; webserver_is_save=1; Guide=1; webserver_lang_type=0; webserver_password=123456");
+    //request.setRawHeader("Host", "172.18.30.19");
+    request.setRawHeader("Referer", "http://172.18.30.19/config/general_cfg.html?0.18188755807976964");
+    request.setRawHeader("session", "0x1ddd480");
+    request.setRawHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko Core/1.70.3676.400 QQBrowser/10.4.3505.400");
+    request.setRawHeader("X-Requested-With", "XMLHttpRequest");
+
+    m_send1->post(request, ui->textEdit_body1->toPlainText().toUtf8());
+}
+
+void MainWindow::on_toolButton_send2_clicked()
+{
+    QNetworkRequest request;
+
+    request.setUrl(QUrl("http://" + ui->lineEdit_IP->text() + ":" + ui->lineEdit_port->text() + ui->lineEdit_package2->text()));
+    request.setRawHeader("Accept", "text/html, */*; q=0.01");
+    request.setRawHeader("Accept-Encoding", "gzip, deflate");
+    request.setRawHeader("Accept-Language", "zh-CN");
+    QString str =  ui->lineEdit_userName->text() + ":" + ui->lineEdit_password->text();
+    request.setRawHeader("Authorization", "Basic " + str.toLocal8Bit().toBase64());
+    request.setRawHeader("Cache-Control", "no-cache");
+    request.setRawHeader("Connection", "Keep-Alive");
+    request.setRawHeader("Content-Length", "0");
+    request.setRawHeader("Content-Type", "application/json; charset=utf-8");
+    request.setRawHeader("Cookie", "webserver_username=admin; webserver_is_save=1; Guide=1; webserver_lang_type=0; webserver_password=123456");
+    //request.setRawHeader("Host", "172.18.30.19");
+    request.setRawHeader("Referer", "http://172.18.30.19/config/general_cfg.html?0.18188755807976964");
+    request.setRawHeader("session", "0x1ddd480");
+    request.setRawHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko Core/1.70.3676.400 QQBrowser/10.4.3505.400");
+    request.setRawHeader("X-Requested-With", "XMLHttpRequest");
+
+    m_send1->post(request, ui->textEdit_body2->toPlainText().toUtf8());
+}
+
+void MainWindow::NetworkReplySend1(QNetworkReply *reply)
+{
+    if(reply->error() == QNetworkReply::NoError)
+    {
+        //ui->textEdit_body1->append(reply->readAll());
+        QString str = reply->readAll();
+        qDebug() << str << endl;
+        ui->textBrowser_show->setText(str);
+    }
+    else
+    {
+        qDebug() << "lallalalal" << endl;
+    }
+}
+
+void MainWindow::NetworkReplySend2(QNetworkReply *reply)
+{
+    if(reply->error() == QNetworkReply::NoError)
+    {
+        //ui->textEdit_body1->append(reply->readAll());
+        QString str = reply->readAll();
+        qDebug() << str << endl;
+        ui->textBrowser_show->setText(str);
+    }
+    else
+    {
+        qDebug() << "lallalalal" << endl;
+    }
 }
